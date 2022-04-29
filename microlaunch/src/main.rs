ea5de93cee48d2fa258848b78ecac294c7ab82ee
Rin <ry00001@protonmail.com>
2022-04-29 09:45:25 +0100

    Prepare for new UI

--- microlaunch/src/main.rs
+++ microlaunch/src/main.rs
@@ -32,14 +32,7 @@ struct CommandLine {
 
 fn run_gui() {
     println!("GUI mode starting...");
-    let app = gui::MicrolaunchApp::default();
-    let native_options = eframe::NativeOptions {
-        decorated: true,
-        transparent: false,
-        min_window_size: Some(egui::vec2(320.0, 10.0)),
-        ..Default::default()
-    };
-    eframe::run_native(Box::new(app), native_options);
+    // TODO: GUI mode
 }
 
 async fn do_full_login_process(data: EncryptedPersistentData) {
