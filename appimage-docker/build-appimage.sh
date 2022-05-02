00846a9da9f26bad57b3ebecadaf3e20b2318bfe
Lilian <lilian@blahaj.space>
2022-05-02 15:19:35 +0200

    Add comments to build scripts

--- appimage-docker/build-appimage.sh
+++ appimage-docker/build-appimage.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
+# As for the three volumes defined below:
+# 1. Mount the source tree into the container for direct usage.
+# 2. Mount a local directoy for cargo build cache, so we can reuse it.
+# 3. Mount a local directory for cargo registry cache, this will probably break at some point?
 run_container() {
     sudo docker run \
     -v $PWD/../:/root/microlaunch/ \
-    -v $PWD/cargo_cache/target/:/root/microlaunch/target \
-    -v $PWD/cargo_cache/registry/:/usr/local/cargo/registry \
+    -v $PWD/cargo_cache/target/:/root/microlaunch/target \ 
+    -v $PWD/cargo_cache/registry/:/usr/local/cargo/registry \ 
     microlaunch-docker
 }
 
+# Check if microlaunch-dcker container already exists. (exit code 0)
 if sudo docker image inspect microlaunch-docker:latest ; [ "$?" -eq 0 ];
 then
     run_container
+
+# If not, we build it, and check if build succeeds. (exit code 0)
 else
-    if cd docker && sudo docker build -t microlaunch-docker:latest . ; [ "$?" -eq 0 ];   
+    if cd docker && sudo docker build -t microlaunch-docker:latest . ; [ "$?" -eq 0 ];
     then
         cd ../ && run_container
     else
--- appimage-docker/docker/build.sh
+++ appimage-docker/docker/build.sh
@@ -2,12 +2,14 @@
 SRC=/root/microlaunch
 APPDIR=/root/AppDir
 cd $SRC
-STEAM_SDK_LOCATION=$SRC/appimage-docker/steamworks_sdk/sdk cargo b --release
+STEAM_SDK_LOCATION=$SRC/appimage-docker/steamworks_sdk/sdk cargo b --release # Build it!
+# Copy files required for AppImage.
 cd $SRC/appimage-docker/assets/
 cp AppRun $APPDIR/
 cp microlaunch.desktop $APPDIR/
 cp icon.png $APPDIR/
 cp $SRC/target/release/microlaunch $APPDIR/usr/bin/
+# We have to include libsteam_api.so for microlaunch to work, so we look for it.
 FP=`find $SRC/target/release/ -name libsteam_api.so`
 cp $FP $APPDIR/usr/lib/
 appimagetool $APPDIR
